24746387f71b508b354c00120d6ddedfbe4a386c
alberto <albertofly10@gmail.com>
2018-01-26 15:24:34 +0100

    Add ability to sort tracks

--- src/main/java/AlbumCreatorController.java
+++ src/main/java/AlbumCreatorController.java
@@ -7,16 +7,22 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class AlbumCreatorController {
 
     @FXML
     private Model model;
-
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -42,6 +48,22 @@ public class AlbumCreatorController {
     @FXML
     private File selectedDirectoryPath;
 
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert upcTextArea != null : "fx:id=\"upcTextArea\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
+        assert chooseFolderButton != null : "fx:id=\"chooseFolderButton\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
+        assert folderPathTextField != null : "fx:id=\"folderPathTextField\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
+        assert itmspCheckbox != null : "fx:id=\"itmspCheckbox\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
+        assert songsNumberTextField != null : "fx:id=\"songsNumberTextField\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
+
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
+
+
     @FXML
     void createAlbumButton(ActionEvent event) {
 
@@ -55,8 +77,6 @@ public class AlbumCreatorController {
             return;
         }
 
-        Scanner scanner = new Scanner(this.upcTextArea.getText());
-
         ButtonType bt = displayConfirmationAlert("Before continuing, make sure you have:\n- Inserted your album covers\n- Placed your tracks inside a folder named Track");
 
         if(bt == ButtonType.OK){
@@ -66,6 +86,31 @@ public class AlbumCreatorController {
             return;
         }
 
+        createFolders();
+
+/*
+        ButtonType bt2 = displayConfirmationAlert("Sorting?");
+
+        if(bt2 == ButtonType.OK){
+            System.out.println("IT WORKS!");
+        } else{
+            System.out.println("CANCELLED");
+            return;
+        }
+*/
+
+        sortTracks();
+
+        //TODO
+        //sortAlbumCovers();
+        //createItmspPackage();
+
+    }
+
+
+    private void createFolders(){
+
+        Scanner scanner = new Scanner(this.upcTextArea.getText());
         while (scanner.hasNextLine()) {
 
             String upc = scanner.nextLine();
@@ -83,37 +128,73 @@ public class AlbumCreatorController {
             }
         }
 
-        File trackDir = new File(selectedDirectoryPath + "/tracks");
+    }
 
-        if(!trackDir.exists()){
+    private void sortTracks(){
+
+        File tracksDir = new File(selectedDirectoryPath + "/tracks");
+
+        if(!tracksDir.exists()){
             displayErrorMessage("tracks folder not created!!");
             return;
         }
 
-        File[] listOfFiles = trackDir.listFiles();
+        int songsPerAlbum = Integer.parseInt(this.songsNumberTextField.getText());
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
+        File[] listOfTrackFiles = tracksDir.listFiles();
+
+        File[] listOfDir = selectedDirectoryPath.listFiles();
+/*        for (File f : listOfDir){
+            System.out.println("dir: " + f.getName());
+        }*/
+
+        List<Integer> randList = new ArrayList<>();
+
+        for(int i = 0; i < listOfTrackFiles.length; i++){
+            randList.add(i);
         }
 
+        System.out.println("original randList: " + randList);
+
+        //first file is _DS STORE!!!!
+        for(int i = 0; i < listOfDir.length; i++){
+
+            if(listOfDir[i].isDirectory() && listOfDir[i].getName().matches("[0-9]{13}")) {
+
+                for (int z = 0; z < songsPerAlbum; z++) {
+
+                    int indexRandom = ThreadLocalRandom.current().nextInt(0, randList.size());
+                    System.out.println("indexRandom: " + indexRandom);
+
+                    int indexRandFile = randList.get(indexRandom);
+
+                    System.out.println("FILE NAME: " + listOfTrackFiles[indexRandFile]);
+
+                    if (listOfTrackFiles[indexRandFile].isFile()) {
+                        System.out.println("MOVING TO: " + listOfDir[i].getAbsolutePath() + "/"+ listOfTrackFiles[indexRandFile].getName()) ;
+
+                        try {
+                            Files.move(Paths.get(listOfTrackFiles[indexRandFile].getAbsolutePath()), Paths.get(listOfDir[i].getAbsolutePath() + "/"+ listOfTrackFiles[indexRandFile].getName()), StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("File " + listOfTrackFiles[indexRandFile].getName());
+                    } else if (listOfTrackFiles[indexRandFile].isDirectory()) {
+                        System.out.println("Directory " + listOfTrackFiles[indexRandFile].getName());
+                    }
+
+                    randList.remove(indexRandom);
+                    System.out.println("randList updated: " + randList);
+
+                }
+            }
+        }
     }
 
 
 
     @FXML
     void chooseFolder(ActionEvent event) {
-
-/*        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Select Folder");
-        this.sourceFolderPath = fileChooser.showOpenDialog(new Stage());
-
-        if(this.sourceFolderPath != null){
-            this.folderPathTextField.setText(this.sourceFolderPath.getAbsolutePath().toString());
-        }*/
 
         DirectoryChooser chooser = new DirectoryChooser();
         chooser.setTitle("Select Folder");
@@ -125,8 +206,6 @@ public class AlbumCreatorController {
         if(selectedDirectoryPath != null){
             this.folderPathTextField.setText(selectedDirectoryPath.getAbsolutePath().toString());
         }
-
-
     }
 
     private void displayErrorMessage(String textMessage) {
@@ -154,22 +233,6 @@ public class AlbumCreatorController {
         alert.getResult();
         alert.showAndWait();
         return alert.getResult();
-
     }
 
-
-
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert upcTextArea != null : "fx:id=\"upcTextArea\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
-        assert chooseFolderButton != null : "fx:id=\"chooseFolderButton\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
-        assert folderPathTextField != null : "fx:id=\"folderPathTextField\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
-        assert itmspCheckbox != null : "fx:id=\"itmspCheckbox\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
-        assert songsNumberTextField != null : "fx:id=\"songsNumberTextField\" was not injected: check your FXML file 'albumCreatorHome.fxml'.";
-
-    }
-
-    public void setModel(Model model) {
-        this.model = model;
-    }
 }
